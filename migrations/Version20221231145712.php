78e3edaa781d94e54145c4f76a5775b64d1ee052
ryang <ryang@etudiant.univ-rennes1.fr>
2022-12-31 16:01:40 +0100

    minor change in the version of migration

---
+++ migrations/Version20221231145712.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221231145712 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE bien ADD url VARCHAR(255) NOT NULL, ADD cp INT DEFAULT NULL, CHANGE titre titre VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE bien DROP url, DROP cp, CHANGE titre titre VARCHAR(255) DEFAULT NULL');
+    }
+}
